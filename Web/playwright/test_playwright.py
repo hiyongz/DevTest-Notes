94f8cf1bbaa416d786c370bc5c7a886e13ed02ae
hiyongz <zhiyo2016@163.com>
2021-10-10 19:19:46 +0800

    add playwright/test_playwright.py

---
+++ Web/playwright/test_playwright.py
@@ -0,0 +1,10 @@
+from playwright.sync_api import sync_playwright
+
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.baidu.com/")
+    print(page.title())
+    browser.close()
