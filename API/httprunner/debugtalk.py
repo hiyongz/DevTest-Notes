f517b9fd803c5bb05837714606f8ffed479cec2f
hiyongz <zhiyo2016@163.com>
2021-10-10 17:14:20 +0800

    add httprunner

---
+++ API/httprunner/debugtalk.py
@@ -0,0 +1,15 @@
+import time
+
+from httprunner import __version__
+
+
+def get_httprunner_version():
+    return __version__
+
+
+def sum_two(m, n):
+    return m + n
+
+
+def sleep(n_secs):
+    time.sleep(n_secs)
