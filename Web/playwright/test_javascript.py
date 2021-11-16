e08b409a2e56ee95650f6394ad3bec2650b57346
hiyongz <zhiyo2016@163.com>
2021-11-16 22:03:28 +0800

    update test_javascript.py

--- Web/playwright/test_javascript.py
+++ Web/playwright/test_javascript.py
@@ -11,40 +11,44 @@ class TestJs():
     def setup(self):
         playwright = sync_playwright().start()
         self.browser = playwright.chromium.launch(headless=False)
-        # self.context = self.browser.new_context()
-        # self.page = self.context.new_page()
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
 
     def teardown(self):
-        self.browser.close()
+        # self.browser.close()
+        pass
 
     def test_case1(self):
-        context = self.browser.new_context()
-        page = context.new_page()
-        page.goto("https://www.baidu.com/")
-        lan = page.evaluate("window.navigator.language;")
-        print(lan)
+        self.page.goto("https://www.baidu.com/")
+        # 获取浏览器语言
+        lan = self.page.evaluate("window.navigator.language;")
         assert lan == "zh-CN"
 
-        title = page.evaluate("document.title;")
-        print(title)
-        assert title == "zh-CN"
+        # 获取当前浏览器标题："百度一下，你就知道"
+        title = self.page.evaluate("document.title;")
+        assert title == self.page.title()
 
-
-        print(page.title())
-
-    def test_input_text(self):
-        context = self.browser.new_context()
-        page = context.new_page()
-        page.goto("https://www.baidu.com/")
-
-        page.evaluate('document.getElementById("kw").value = "test"')
-        text = page.evaluate("document.getElementById('kw').value")
+        self.page.evaluate('document.getElementById("kw").value = "test"') # 搜索框输入“test”
+        text = self.page.evaluate("document.getElementById('kw').value") # 读取输入的值
         assert text == "test"
 
-        page.evaluate('document.getElementById("su").click()') # page.click("text=百度一下")
-        page.click("#page >> text=2")
+        self.page.evaluate('document.getElementById("su").click()')  # 点击 【百度一下】；page.click("text=百度一下")
+        self.page.click("#page >> text=2")
 
+        time.sleep(5)
 
+    def test_case2(self):
+        self.page.goto("https://www.baidu.com/")
 
-        # browser.close()
-        time.sleep(30)
+        # 搜索框输入“test”
+        input_handle = self.page.query_selector("id=kw")
+        input_handle.evaluate('node => node.value = "test"')
+
+        text = input_handle.evaluate('node => node.value') # 读取输入的值
+        print(text)
+        assert text == "test"
+
+        # 点击 【百度一下】
+        submit_handle = self.page.query_selector("id=su")
+        submit_handle.evaluate('node => node.click()')
+        time.sleep(10)
