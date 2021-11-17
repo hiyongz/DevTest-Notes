763858f7f7046f41953d30037571327dc681b757
hiyongz <zhiyo2016@163.com>
2021-11-17 18:05:27 +0800

    update test_browser_language.py

--- Web/playwright/test_browser_language.py
+++ Web/playwright/test_browser_language.py
@@ -18,10 +18,7 @@ class TestBrowser():
 
     def test_chrome(self):
         self.browser = self.playwright.chromium.launch(channel="chrome", headless=False)
-        # context = self.browser.new_context(locale="es")
-        # context = self.browser.new_context(locale="en-GB")
-        # context = self.browser.new_context(locale="es")
-        context = self.browser.new_context(locale="zh-CN")
+        context = self.browser.new_context(locale="zh-CN") # zh-CN、en-GB
         page = context.new_page()
         page.goto("https://www.baidu.com/")
         lan = page.evaluate("window.navigator.language;")
@@ -32,25 +29,37 @@ class TestBrowser():
         print(page.title())
 
     def test_edge(self):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(channel="msedge", headless=False)
-            # context = browser.new_context(locale="zh-CN")
-            # context = browser.new_context(locale="en-GB")
-            context = browser.new_context(locale="de-DE")
-            page = context.new_page()
-            page.goto("https://www.baidu.com/")
-            print(page.title())
+        self.browser = self.playwright.chromium.launch(channel="msedge", headless=False)
+        context = self.browser.new_context(locale="de-DE")
+        page = context.new_page()
+        page.goto("https://www.baidu.com/")
+        lan = page.evaluate("window.navigator.language;")
+        page.goto("https://www.ip-com.com.cn/")
+        time.sleep(3)
+        print(lan)
+        assert lan == "zh-CN"
+        print(page.title())
 
     def test_firefox(self):
-        with sync_playwright() as p:
-            browser = p.firefox.launch(headless=False)
-            page = browser.new_page()
-            page.goto("https://www.baidu.com/")
-            print(page.title())
+        self.browser = self.playwright.firefox.launch(headless=False)
+        context = self.browser.new_context(locale="de-DE")
+        page = context.new_page()
+        page.goto("https://www.baidu.com/")
+        lan = page.evaluate("window.navigator.language;")
+        page.goto("https://www.ip-com.com.cn/")
+        time.sleep(3)
+        print(lan)
+        assert lan == "zh-CN"
+        print(page.title())
 
     def test_webkit(self):
-        with sync_playwright() as p:
-            browser = p.webkit.launch(headless=False)
-            page = browser.new_page()
-            page.goto("https://www.baidu.com/")
-            print(page.title())
+        self.browser = self.playwright.webkit.launch(headless=False)
+        context = self.browser.new_context(locale="de-DE")
+        page = context.new_page()
+        page.goto("https://www.baidu.com/")
+        lan = page.evaluate("window.navigator.language;")
+        page.goto("https://www.ip-com.com.cn/")
+        time.sleep(3)
+        print(lan)
+        assert lan == "zh-CN"
+        print(page.title())  
